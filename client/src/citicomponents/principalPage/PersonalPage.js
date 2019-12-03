55a2bbb04e00f63ce33dfe579d7b49ea31176efd
Rosa Isela Lopez Palma <rosaiselalopezpalma@air-de-rosa>
2019-12-03 09:13:26 -0600

    save navbar dashboard

---
+++ client/src/citicomponents/principalPage/PersonalPage.js
@@ -0,0 +1,18 @@
+import React from 'react'
+
+const PersonalPage = () => {
+  return (
+    <div>
+      <div className='whithe-nav'>
+        <div className='div-hover'><b>Cuentas</b></div>
+        <div className='div-hover'><b>Transferencias y Pagos</b></div>
+        <div className='div-hover'><b>Inversiones</b></div>
+        <div className='div-hover'><b>Otros productos</b></div>
+        <div className='div-hover'><b>Configuración</b></div>      
+    </div>
+      
+    </div>
+  )
+}
+
+export default PersonalPage;
