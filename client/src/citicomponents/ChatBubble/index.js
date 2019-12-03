3cfa141156682ff369ff214d3cc272efdfa230d1
@sandyGonzalez <sandygonzalez.612@gmail.com>
2019-12-03 09:02:00 -0600

    Agregando cambios

---
+++ client/src/citicomponents/ChatBubble/index.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import CommunicationChat from '../../img/citi-communication-chat.png';
+
+const ChatBubble = ()=>{
+
+    return(
+        <section>
+            <img src = {CommunicationChat}/>
+        </section>
+    );
+}
+
+export default ChatBubble
